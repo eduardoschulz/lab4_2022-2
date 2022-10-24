c6603d08e4fe23a465ccad5bd5bbbac9131f5df8
Eduardo Schulz <eduardo.schulz@protonmail.com>
2022-10-24 20:44:28 -0300

    arrumando classe vacinacao e add testes a mesma

--- src/main/java/emergencia/Vacinacao.java
+++ src/main/java/emergencia/Vacinacao.java
@@ -7,30 +7,45 @@ import fila.Fila;
 import java.util.ArrayList;
 
 public class Vacinacao {
-    public static void main(String[] args) {
-        ComparadorIdade condicao = new ComparadorIdade();
-        Fila entrada = new Fila(condicao);
-        Fila saida = new Fila(condicao);
-        ArrayList<Paciente> www = new ArrayList();
-        Paciente veio1 = new Paciente(92, "a");
-        Paciente jovem1 = new Paciente(12, "b");
-        Paciente veio2 = new Paciente(291, "c");
+    ComparadorIdade condicao = new ComparadorIdade();
+    Fila entrada = new Fila(condicao);
+    Fila saida = new Fila(condicao);
 
-        entrada.getFila().add(veio1);
-        entrada.getFila().add(jovem1);
-        entrada.getFila().add(veio2);
+    public ComparadorIdade getCondicao() {
+        return condicao;
+    }
 
-        www.add(veio1);
-        www.add(veio2);
-        www.add(jovem1);
-        System.out.printf("%s\n", www.get(1).getNome());
-        while (!entrada.getFila().isEmpty()){
-           Paciente vacinado= entrada.getFila().poll();
-           saida.getFila().add(vacinado);
-           System.out.printf("%s\n", vacinado.getNome());
+    public void setCondicao(ComparadorIdade condicao) {
+        this.condicao = condicao;
+    }
+
+    public Fila getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(Fila entrada) {
+        this.entrada = entrada;
+    }
+
+    public Fila getSaida() {
+        return saida;
+    }
+
+    public void setSaida(Fila saida) {
+        this.saida = saida;
+    }
+
+    public void vacinacao(Paciente... pacientes){
+
+
+        for (Paciente paciente:pacientes) {
+           entrada.getFila().add(paciente);
         }
 
-
+        while (!entrada.getFila().isEmpty()) {
+            Paciente vacinado = entrada.getFila().poll();
+            saida.getFila().add(vacinado);
+        }
     }
 
 
--- src/test/java/emergencia_teste/VacinacaoTeste.java
+++ src/test/java/emergencia_teste/VacinacaoTeste.java
@@ -1,10 +1,29 @@
 package emergencia_teste;
 
+import emergencia.Paciente;
+import emergencia.Vacinacao;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class VacinacaoTeste {
     @Test
-    public void testeDeOrdenacao(){
+    public void testeOrdem(){
+        Vacinacao vax = new Vacinacao();
 
+        Paciente pac1 = new Paciente(10, "10");
+        Paciente pac2 = new Paciente(20, "20");
+        Paciente pac3 = new Paciente(100, "100");
+        vax.vacinacao(pac1,pac2,pac3);
+        Assert.assertEquals(vax.getSaida().getFila().poll(), pac3);
+    }
+    @Test
+    public void isEntradaVazia(){
+        Vacinacao vax = new Vacinacao();
+        Paciente pac1 = new Paciente(10, "10");
+        Paciente pac2 = new Paciente(20, "20");
+        Paciente pac3 = new Paciente(100, "100");
+
+        vax.vacinacao(pac1,pac2,pac3);
+        Assert.assertEquals(vax.getEntrada().getFila().isEmpty(), true);
     }
 }
