dec316d378aef850716e4d48b52eca81a8ab9b67
Eduardo Schulz <33841225+eduardoschulz@users.noreply.github.com>
2022-11-22 02:11:31 +0000

    add main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,4 +1,18 @@
 package main;
 
+import emergencia.Vacinacao;
+
 public class Main {
+    public static void main(String[] args) {
+        
+    
+    Vacinacao vax = new Vacinacao();
+
+    Paciente pac1 = new Paciente(10, "10");
+    Paciente pac2 = new Paciente(20, "20");
+    Paciente pac3 = new Paciente(100, "100");
+    vax.vacinacao(pac1,pac2,pac3);
+    }
+
+
 }
